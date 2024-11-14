26445b61d9d477099a242e782430064bbfe33ab5
Andreina2 <andreinaarias5@gmail.com>
2024-11-14 15:24:09 +0100

    update Readme.md

---
+++ src/main/java/dev/andreina/ex_bmi_calculator/AppBMI.java
@@ -0,0 +1,14 @@
+package dev.andreina.ex_bmi_calculator;
+
+/**
+ * Hello world!
+ */
+public final class AppBMI {
+    public static void main(String[] args) {
+        System.out.println("                         ");
+        System.out.println("Welcome to BMI calculator");
+        System.out.println("-------------------------");
+        System.out.println("                         ");
+    }
+}
+
