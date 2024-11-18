5ea80707f0a80b59d4c40217dfc7b752c7d9d62b
Andreina2 <andreinaarias5@gmail.com>
2024-11-18 16:15:18 +0100

    Create BmiModelTest

---
+++ src/test/java/dev/andreina/ex_bmi_calculator/BmiModelTest.java
@@ -0,0 +1,16 @@
+package dev.andreina.ex_bmi_calculator;
+
+import org.junit.jupiter.api.Test;
+
+public class BmiModelTest {
+    @Test
+    void testCalculateBMI() {
+        
+
+    }
+
+    @Test
+    void testGiveResults() {
+
+    }
+}
