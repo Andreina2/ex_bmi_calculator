d01266f97f05dd38ec4c128975e62060fb5d4a01
Andreina2 <andreinaarias5@gmail.com>
2024-11-19 17:52:02 +0100

    update comments

--- src/main/java/dev/andreina/ex_bmi_calculator/BMIController.java
+++ src/main/java/dev/andreina/ex_bmi_calculator/BMIController.java
@@ -32,15 +32,4 @@ public class BMIController {
 
     }
 
-    /*Para hacer el controller podéis inspiraros con BmiModel ,la secuencia es similar */
-
-    //Atributos: buscamos traer la lógica del BMIModel y la interacción de BmiView
-
-
-
-    //Constructor o constructores: que parámetros voy a necesitar pasar??
-
-    
-
-    //Métodos (acciones que debe realizar)
 }
